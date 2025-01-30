63b825c4cdbfaad4e5427557cdddc58ea7309c6a
조창훈 <xpfjsdmlghkd@gmail.com>
2025-01-30 20:19:53 +0900

    hotfix: webHook 관련 발생 에러 처리

--- backend/rest/src/main/java/com/giteat/webHook/gitLab/service/GitLabWebHookServiceImpl.java
+++ backend/rest/src/main/java/com/giteat/webHook/gitLab/service/GitLabWebHookServiceImpl.java
@@ -3,21 +3,22 @@ package com.giteat.webHook.gitLab.service;
 import com.giteat.webHook.gitLab.entity.GitLabCommitEntity;
 import com.giteat.webHook.gitLab.entity.GitLabMergeRequestEntity;
 import com.giteat.webHook.gitLab.entity.GitLabNoteEntity;
-import com.giteat.webHook.gitLab.repository.GitLabWebHookRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.giteat.webHook.gitLab.repository.GitLabCommitRepository;
+import com.giteat.webHook.gitLab.repository.GitLabMergeRequestRepository;
+import com.giteat.webHook.gitLab.repository.GitLabNoteRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
+@AllArgsConstructor
 public class GitLabWebHookServiceImpl implements GitLabWebHookService{
 
-    private final GitLabWebHookRepository gitLabRepository;
+    private final GitLabCommitRepository gitLabCommitRepository;
+    private final GitLabNoteRepository gitLabNoteRepository;
+    private final GitLabMergeRequestRepository gitLabMergeRequestRepository;
 
-    @Autowired
-    public GitLabWebHookServiceImpl(GitLabWebHookRepository gitLabWebHookRepository){
-        this.gitLabRepository = gitLabWebHookRepository;
-    }
 
     //TODO : 현재 데이터를 받아서 저장하는 로직만 설정해놓은 상태이다. 이 상태에서 추가 더 필요 데이터가 있을 경우 처리해서 저장 해야한다.
 
@@ -30,7 +31,7 @@ public class GitLabWebHookServiceImpl implements GitLabWebHookService{
     public void commitEvent(Map<String, Object> body) {
         GitLabCommitEntity commitEntity = new GitLabCommitEntity();
         //데이터 정제하는 코드 추가 필요함
-        gitLabRepository.save(commitEntity);
+        gitLabCommitRepository.save(commitEntity);
     }
 
     /**
@@ -41,7 +42,7 @@ public class GitLabWebHookServiceImpl implements GitLabWebHookService{
     public void mergeRequestEvent(Map<String, Object> body) {
         GitLabMergeRequestEntity mergeRequestEntity = new GitLabMergeRequestEntity();
         //데이터 정제하는 코드 추가 필요함
-        gitLabRepository.save(mergeRequestEntity);
+        gitLabMergeRequestRepository.save(mergeRequestEntity);
     }
 
     /**
@@ -52,6 +53,6 @@ public class GitLabWebHookServiceImpl implements GitLabWebHookService{
     public void noteEvent(Map<String, Object> body) {
         GitLabNoteEntity noteEntity = new GitLabNoteEntity();
         //데이터 정제하는 코드 추가 필요함
-        gitLabRepository.save(noteEntity);
+        gitLabNoteRepository.save(noteEntity);
     }
 }
