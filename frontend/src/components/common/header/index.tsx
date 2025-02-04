b82c1aff7f4244c45adb89c8222eba8f2947a779
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-02-04 16:20:24 +0900

    fix: 이미지 경로 수정

--- frontend/src/components/common/header/index.tsx
+++ frontend/src/components/common/header/index.tsx
@@ -1,5 +1,26 @@
 import { useMemo } from "react";
 import { Link, useLocation } from "react-router-dom";
+import defaulProfile from "../../../assets/images/default_user.png";
+import whiteLogo from "../../../assets/images/logo_white.svg";
+import home from "../../../assets/images/home.svg";
+import darkHome from "../../../assets/images/home_dark.svg";
+import file from "../../../assets/images/file.svg";
+import darkFile from "../../../assets/images/file_dark.svg";
+import wiki from "../../../assets/images/wiki.svg";
+import darkWiki from "../../../assets/images/wiki_dark.svg";
+import report from "../../../assets/images/reports.svg";
+import darkReport from "../../../assets/images/reports_dark.svg";
+
+const images: Record<string, string> = {
+  home: home,
+  darkhome: darkHome,
+  file: file,
+  darkfile: darkFile,
+  wiki: wiki,
+  darkwiki: darkWiki,
+  report: report,
+  darkreport: darkReport,
+};
 
 function LinkIcon({ to, src, alt }: { to: string; src: string; alt: string }) {
   const location = useLocation();
@@ -17,7 +38,7 @@ function LinkIcon({ to, src, alt }: { to: string; src: string; alt: string }) {
     >
       <img
         className={`w-[30px]`}
-        src={`/src/assets/images/${isSelected ? src + "_dark" : src}.svg`}
+        src={isSelected ? images["dark" + src] : images[src]}
         alt={alt}
       />
     </Link>
@@ -27,18 +48,14 @@ function LinkIcon({ to, src, alt }: { to: string; src: string; alt: string }) {
 export function Header() {
   return (
     <header className="h-[100vh] bg-black w-[115px] flex flex-col justify-between items-center box-border py-[36px] fixed">
-      <img
-        src="/src/assets/images/default_user.png"
-        alt="user profile"
-        className="w-[51px]"
-      />
+      <img src={defaulProfile} alt="user profile" className="w-[51px]" />
       <div className="flex flex-col gap-[50px] items-center">
         <LinkIcon to="/repos" src="home" alt="repository_page" />
         <LinkIcon to="/pulls" src="file" alt="pr_page" />
         <LinkIcon to="/wiki" src="wiki" alt="dashboard_page" />
-        <LinkIcon to="/dashboard" src="reports" alt="dashboard_page" />
+        <LinkIcon to="/dashboard" src="report" alt="dashboard_page" />
       </div>
-      <img src="/src/assets/images/logo_white.svg" alt="logout" />
+      <img src={whiteLogo} alt="logout" />
     </header>
   );
 }
--- frontend/src/components/pullRequest/header/index.tsx
+++ frontend/src/components/pullRequest/header/index.tsx
@@ -1,12 +1,9 @@
+import book from "../../../assets/images/image.png";
 export function Header() {
   return (
     <header className="w-full p-4">
       <div className="flex items-center gap-2 align-center">
-        <img
-          src="/src/assets/images/image.png"
-          alt="pull request page"
-          className="w-[18px]"
-        />
+        <img src={book} alt="pull request page" className="w-[18px]" />
         <h1 className="text-[18px] font-semibold flex gap-2 text-center pb-1">
           <span>TakeGitEasy</span>
           <span>/</span>
