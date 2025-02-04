cde34a75746727632d16d93420fdb594e2628447
조창훈 <ckdgns6078@naver.com>
2025-02-04 15:09:30 +0900

    fix: CORS 경로 설정

--- backend/rest/src/main/java/com/giteat/common/config/CorsConfig.java
+++ backend/rest/src/main/java/com/giteat/common/config/CorsConfig.java
@@ -16,6 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
                 .allowedMethods("*") // 모든 HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보 포함 허용
+
     }
 }
 
