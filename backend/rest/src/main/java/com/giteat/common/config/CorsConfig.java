021312e2d13992b47c09ec3cc142abce3f0330ec
조창훈 <ckdgns6078@naver.com>
2025-02-04 15:08:09 +0900

    fix: CORS 경로 설정

--- backend/rest/src/main/java/com/giteat/common/config/CorsConfig.java
+++ backend/rest/src/main/java/com/giteat/common/config/CorsConfig.java
@@ -11,13 +11,11 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // 모든 경로에 대해 CORS 허용
-                .allowedOrigins(
-                        "http://i12b108.p.ssafy.io:8801",
-                        "http://i12b108.p.ssafy.io"
-                ) // 허용할 Origin 설정
+        registry.addMapping("/**")  // 모든 경로 허용
+                .allowedOriginPatterns("http://i12b108.p.ssafy.io:*") // 모든 포트 허용
                 .allowedMethods("*") // 모든 HTTP 메서드 허용
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보 포함 허용
     }
 }
+
