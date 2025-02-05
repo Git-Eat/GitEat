8df628e27cdfb6fb28968d05d6837e698b2fc36a
이해루 <gofn080776@gmail.com>
2025-02-06 00:22:27 +0900

    feat: 모달 레이아웃 작성

--- frontend/src/pages/repositoryList/index.tsx
+++ frontend/src/pages/repositoryList/index.tsx
@@ -1,7 +1,10 @@
 import add from "../../assets/images/add.svg";
 import { RepositoryCard } from "../../components/repositoryList/RepositoryCard";
 import { DummyRepos } from "./dummy";
+import { useBooleanState } from "../../hooks/useBooleanState";
+import RepositoryAddModal from "../../components/repositoryList/repositoryAddModal";
 export function RepositoryList() {
+  const [isModalOpen, openModal, closeModal] = useBooleanState(false);
   return (
     <>
       <header className="w-full p-4">
@@ -14,14 +17,10 @@ export function RepositoryList() {
       </header>
       <main className=" w-[98%] m-auto px-8 py-4 bg-gray-100 rounded-2xl min-h-[calc(100vh-100px)]">
         <div className="flex flex-col gap-5 m-auto w-[80%] pt-10">
-          <div className="flex gap-2  justify-end">
-            <label htmlFor="add-repository" className="hover:cursor-pointer">
-              프로젝트 추가하기{" "}
-            </label>
-            <button id="add-repository">
-              <img src={add} alt="add" />
-            </button>
-          </div>
+          <button className="flex gap-2  justify-end" onClick={openModal}>
+            <span className="hover:cursor-pointer">프로젝트 추가하기 </span>
+            <img src={add} alt="add" />
+          </button>
           {DummyRepos.map((repo, idx) => (
             <RepositoryCard
               key={idx}
@@ -32,6 +31,7 @@ export function RepositoryList() {
           ))}
         </div>
       </main>
+      <RepositoryAddModal closeModal={closeModal} isModalOpen={isModalOpen} />
     </>
   );
 }
