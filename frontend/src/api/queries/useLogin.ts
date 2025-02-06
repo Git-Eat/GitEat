06439342221a417cbe105ebea9598f9b88e90498
이해루 <gofn080776@gmail.com>
2025-02-06 21:12:58 +0900

    hotfix: 로깅 위치 변경

--- frontend/src/api/queries/useLogin.ts
+++ frontend/src/api/queries/useLogin.ts
@@ -1,14 +1,17 @@
 import { useMutation } from "react-query";
 import { AuthToken } from "../types/Auth.ts";
 import { login } from "../auth.ts";
+import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
+  const navagation = useNavigate();
   return useMutation(login, {
     mutationKey: "login",
     onSuccess: (data: AuthToken) => {
       alert("login success");
       localStorage.setItem("access_token", data.accessToken);
-      window.location.replace("/dashboard");
+      navagation("/dashboard");
+      // window.location.replace("/dashboard");
     },
     retry: false,
   });
