df698c28087a6f0fe02f7bb1794e26061f032c19
조창훈 <ckdgns6078@naver.com>
2025-02-06 09:30:20 +0900

    feat: swagger 추가 및 noteEntity 삭제

--- backend/rest/src/main/java/com/giteat/pr/controller/PrController.java
+++ backend/rest/src/main/java/com/giteat/pr/controller/PrController.java
@@ -204,16 +204,4 @@ public class PrController {
         return ResponseEntity.noContent().build();
     }
 
-    /**
-     * 등록한 레포지토리 정보 읽어 오기
-     * @param accessToken
-     * @param repositoryId
-     * @return
-     */
-    @PostMapping("/repositoryData")
-    @Operation(summary="repository의 모든 데이터 읽기", description = "repository에서 모든 데이터를 가져옵니다.")
-    public ResponseEntity<?> saveRepositoryData(@RequestHeader("accessToken") String accessToken , @RequestBody String repositoryId){
-        prService.saveRepositoryData(accessToken, repositoryId);
-        return ResponseEntity.ok().build();
-    }
 }
--- backend/rest/src/main/java/com/giteat/repo/service/RepoService.java
+++ backend/rest/src/main/java/com/giteat/repo/service/RepoService.java
@@ -9,4 +9,5 @@ public interface RepoService {
     RepositoryEntity findByRepoId(int repoId);
     RepositoryEntity insertRepo(int repoId);
     int deleteRepo(int repoId);
+    int saveRepositoryData(String accessToken , String repositoryId);
 }
