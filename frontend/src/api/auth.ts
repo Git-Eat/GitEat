3080ff07a7921d858b764df368e6beb094a8a2fd
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-02-07 12:38:27 +0900

    refactor: 로깅용 alert 제거

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -2,7 +2,6 @@ import client from "./client";
 
 export const login = async (code: string) => {
   try {
-    alert("auth.ts");
     const response = await client.post("/oauth/gitlab/login", { code: code });
     return response.data;
   } catch (error) {
