92167c265f12d62546852f3e83822261d7ad87ff
이해루 <gofn080776@gmail.com>
2025-02-08 01:01:25 +0900

    feat: 쿼리키 유니크로 수정

--- frontend/src/api/queries/useGetPullRequest.ts
+++ frontend/src/api/queries/useGetPullRequest.ts
@@ -2,9 +2,13 @@ import { useQuery } from "react-query";
 import { getPullRequest } from "../pullRequest";
 
 export const useGetPullRequest = (repoId: number, prId: number) => {
-  return useQuery("getPullRequest", () => getPullRequest(repoId, prId), {
-    staleTime: 1000 * 60 * 5,
-    useErrorBoundary: true,
-    // suspense: true,
-  });
+  return useQuery(
+    ["getPullRequest" + repoId + prId],
+    () => getPullRequest(repoId, prId),
+    {
+      staleTime: 1000 * 60 * 5,
+      useErrorBoundary: true,
+      // suspense: true,
+    }
+  );
 };
--- frontend/src/api/queries/useGetPullRequests.ts
+++ frontend/src/api/queries/useGetPullRequests.ts
@@ -2,7 +2,7 @@ import { useQuery } from "react-query";
 import { getPullRequests } from "../pullRequest";
 
 export const useGetPullRequests = (repoId: number) => {
-  return useQuery(["getPullRequests"], () => getPullRequests(repoId), {
+  return useQuery(["getPullRequests", repoId], () => getPullRequests(repoId), {
     staleTime: 1000 * 60 * 5,
     retry: 0,
     suspense: true,
