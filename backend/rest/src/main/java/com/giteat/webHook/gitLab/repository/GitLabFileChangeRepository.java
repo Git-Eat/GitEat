6fc8c2cfff67f8fa3695da98d93caafc711490f2
조창훈 <xpfjsdmlghkd@gmail.com>
2025-02-10 22:34:07 +0900

    hotfix: webHook관련 주석처리

--- backend/rest/src/main/java/com/giteat/webHook/gitLab/repository/GitLabFileChangeRepository.java
+++ backend/rest/src/main/java/com/giteat/webHook/gitLab/repository/GitLabFileChangeRepository.java
@@ -1,7 +1,7 @@
-package com.giteat.webHook.gitLab.repository;
-
-import com.giteat.webHook.gitLab.entity.GitLabFileChangeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GitLabFileChangeRepository extends JpaRepository<GitLabFileChangeEntity, Long> {
-}
+//package com.giteat.webHook.gitLab.repository;
+//
+//import com.giteat.webHook.gitLab.entity.GitLabFileChangeEntity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface GitLabFileChangeRepository extends JpaRepository<GitLabFileChangeEntity, Long> {
+//}
