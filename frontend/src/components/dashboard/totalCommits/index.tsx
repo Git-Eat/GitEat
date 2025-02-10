92b95360295f720db6f1736eb95a1903f97a7095
이해루 <gofn080776@gmail.com>
2025-02-11 02:55:33 +0900

    feat: 총 커밋수 컴포넌트 추가

---
+++ frontend/src/components/dashboard/totalCommits/index.tsx
@@ -0,0 +1,8 @@
+export function TotalCommits() {
+  return (
+    <div>
+      <span>Commit 수</span>
+      <span>+3.234개</span>
+    </div>
+  );
+}
--- frontend/src/pages/dashboard/index.tsx
+++ frontend/src/pages/dashboard/index.tsx
@@ -1,3 +1,23 @@
+import { Header } from "../../components/pullRequest/header";
+import { TotalCommits } from "../../components/dashboard/totalCommits";
+
 export function DashBoard() {
-  return <>대시보드 페이지</>;
+  return (
+    <div>
+      <Header title={"asdf"} owner={"asdfasdf"} />
+
+      <main className="w-[98%] m-auto px-8 py-4 bg-gray-100 rounded-2xl min-h-[calc(100vh-100px)]">
+        <div className="w-[80%] m-auto">
+          <h1 className=" text-2xl font-semibold flex gap-4 text-center pb-1">
+            <span>DashBoard</span>
+          </h1>
+          <span className="block text-neutral-400 text-sm">
+            <span className="text-black">asdf</span> 의 프로젝트 현황을
+            확인해보세요!
+          </span>
+          <TotalCommits />
+        </div>
+      </main>
+    </div>
+  );
 }
