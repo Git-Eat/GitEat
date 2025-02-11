086d01b34df4505f97b02f399123f5c587435272
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-02-11 17:26:41 +0900

    hotfix: 토큰에러 수정

--- frontend/src/api/queries/useLogin.ts
+++ frontend/src/api/queries/useLogin.ts
@@ -1,5 +1,4 @@
 import { useMutation } from "react-query";
-import { AuthToken } from "../types/Auth.ts";
 import { login } from "../auth.ts";
 import { useNavigate } from "react-router-dom";
 
@@ -7,8 +6,9 @@ export const useLogin = () => {
   const navigation = useNavigate();
   return useMutation(login, {
     mutationKey: "login",
-    onSuccess: (data: AuthToken) => {
-      localStorage.setItem("access_token", data.accessToken);
+    onSuccess: (accessToken: string) => {
+      console.log(accessToken);
+      localStorage.setItem("access_token", accessToken);
       navigation("/dashboard", { replace: true });
     },
     retry: false,
