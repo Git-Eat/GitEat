668788493c7bf3fdd9fdc5a1e41a279ba043697d
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-02-11 17:30:46 +0900

    hotfix: 토큰 로깅 테스트

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -3,6 +3,7 @@ import client from "./client";
 export const login = async (code: string) => {
   try {
     const response = await client.post("/oauth/gitlab/login", { code: code });
+    console.log(response.headers);
     return response.headers["Authorization"];
   } catch (error) {
     throw new Error("로그인에 실패했습니다." + error);
