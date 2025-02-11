6cb8618bf0c7e0f26796e69fab7e5e1a0dba0235
조창훈 <xpfjsdmlghkd@gmail.com>
2025-02-11 21:59:11 +0900

    hotfix: postHandle 관련 try catch 에러출력

--- backend/security/src/main/java/com/giteat/security/interceptor/OauthInterceptor.java
+++ backend/security/src/main/java/com/giteat/security/interceptor/OauthInterceptor.java
@@ -80,7 +80,7 @@ public class OauthInterceptor implements HandlerInterceptor {
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
         String requestURI = request.getRequestURI();
         System.out.println("종료 경로 : " + requestURI);
         if(requestURI.startsWith("/api/oauth/gitlab/login")){
@@ -94,18 +94,25 @@ public class OauthInterceptor implements HandlerInterceptor {
         System.out.println("새로 만들어서 반환 access : " + accessToken);
         System.out.println("새로 만들어서 반환 refresh : " + refreshToken);
         // cookie 설정
-        Cookie cookie = new Cookie("refreshToken", refreshToken);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setMaxAge(maxAge);
 
-        response.addCookie(cookie);
+        try{
+            Cookie cookie = new Cookie("refreshToken", refreshToken);
+            cookie.setHttpOnly(true);
+            cookie.setSecure(true);
+            cookie.setPath("/");
+            cookie.setMaxAge(maxAge);
 
-        // accessToken을 HTTP 응답 헤더에 추가
-        response.setHeader("Authorization", "Bearer " + accessToken);
-        TokenContext.removeAccessToken();
-        TokenContext.removeRefreshToken();
+            response.addCookie(cookie);
+
+            // accessToken을 HTTP 응답 헤더에 추가
+            response.setHeader("Authorization", "Bearer " + accessToken);
+            TokenContext.removeAccessToken();
+            TokenContext.removeRefreshToken();
+            System.out.println("모든 postHandle 실행완료");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 
     public void responseSetting(HttpServletResponse response) throws IOException {
