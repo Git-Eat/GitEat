ae919850112ebaaef9238d92596c8abb972d2e3f
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-02-11 17:21:40 +0900

    hotfix: 토큰 관련 에러 수정

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -3,7 +3,7 @@ import client from "./client";
 export const login = async (code: string) => {
   try {
     const response = await client.post("/oauth/gitlab/login", { code: code });
-    return response.headers["accessToken"];
+    return response.headers["Authorization"];
   } catch (error) {
     throw new Error("로그인에 실패했습니다." + error);
   }
--- frontend/src/api/authClient.ts
+++ frontend/src/api/authClient.ts
@@ -11,7 +11,7 @@ const authClient = axios.create({
 authClient.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`;
+    config.headers["Authorization"] = `${token}`;
   }
   return config;
 });
