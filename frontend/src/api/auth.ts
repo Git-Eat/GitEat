e3a1f15a2e57210790eb6693174a875b259cc02c
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-02-11 17:13:13 +0900

    feat: 로그인 액세스 토큰 수신 방식 변경

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -3,7 +3,7 @@ import client from "./client";
 export const login = async (code: string) => {
   try {
     const response = await client.post("/oauth/gitlab/login", { code: code });
-    return response.data;
+    return response.headers["accessToken"];
   } catch (error) {
     throw new Error("로그인에 실패했습니다." + error);
   }
