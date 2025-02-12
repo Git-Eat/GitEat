171676e67b5c4d64d3ad383acc3574a3d81479e3
송용인 <78163645+Lil-Young@users.noreply.github.com>
2025-02-13 00:04:00 +0900

    fix: ReportController Dto 로그 추가 및 Dto 변수 변경

--- backend/security/src/main/java/com/giteat/security/controller/ReportController.java
+++ backend/security/src/main/java/com/giteat/security/controller/ReportController.java
@@ -24,6 +24,15 @@ public class ReportController {
     public ResponseEntity<?> LighthouseSend(
             @RequestBody LighthouseDto lighthouseDto) {
         log.info("🔹 POST /api/report/lighthouse-pipeline 요청 받음! lighthouseDto: {}", lighthouseDto);
+
+        // ✅ 🔥 각 필드별 로그 출력 (null 체크)
+        log.info("▶ gitUrl: {}", lighthouseDto.getGitUrl());
+        log.info("▶ branch: {}", lighthouseDto.getBranch());
+        log.info("▶ repoId: {}", lighthouseDto.getRepoId());
+        log.info("▶ build: {}", lighthouseDto.getBuild());
+        log.info("▶ frontendPath: {}", lighthouseDto.getFrontendPath());
+
+
         ResponseEntity<?> response = apiUtl.postReportApi("/lighthouse-pipeline" , lighthouseDto);
 
         return ResponseEntity.ok(response.getBody());
