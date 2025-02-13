ea584b2672b12b8ac02dadf7409993bb4fa3cd2f
이해루 <gofn080776@gmail.com>
2025-02-14 01:03:48 +0900

    hotfix: 사용하지 않는 아이콘 제거

--- frontend/src/components/common/header/index.tsx
+++ frontend/src/components/common/header/index.tsx
@@ -10,6 +10,7 @@ import wiki from "../../../assets/images/wiki.svg";
 import darkWiki from "../../../assets/images/wiki_dark.svg";
 import report from "../../../assets/images/reports.svg";
 import darkReport from "../../../assets/images/reports_dark.svg";
+import { useLoginStore } from "../../../store/loginStore";
 
 const images: Record<string, string> = {
   home: home,
@@ -45,9 +46,14 @@ function LinkIcon({ to, src, alt }: { to: string; src: string; alt: string }) {
 }
 
 export function Header() {
+  const { user } = useLoginStore();
   return (
     <header className="h-[100vh] bg-black w-[115px] flex flex-col justify-between items-center box-border py-[36px] fixed">
-      <img src={defaulProfile} alt="user profile" className="w-[51px]" />
+      <img
+        src={user.avatar_url}
+        alt="user profile"
+        className="w-[51px] box-border rounded-xl"
+      />
       <div className="flex flex-col gap-[50px] items-center">
         <LinkIcon to="/repos" src="home" alt="repository_page" />
         {/* <LinkIcon to="/pulls" src="file" alt="pr_page" />
