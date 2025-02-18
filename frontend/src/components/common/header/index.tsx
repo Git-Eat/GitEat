be8b668d2445d4cf681f662ff5b87f164b9b65f7
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-02-18 12:56:52 +0900

    style: header 셀렉터 패딩 추가

--- frontend/src/components/common/header/index.tsx
+++ frontend/src/components/common/header/index.tsx
@@ -22,7 +22,7 @@ export function Header() {
         <Link
           to="/repos"
           onClick={() => setSelect("/repos")}
-          className={`rounded-[12px] w-full px-[5px] py-[5px] hover:bg-[#dce9ff] transition ease-in-out delay-50 ${isSelected === "/repos" ? "bg-[#dce9ff]" : "bg-white"}`}
+          className={`rounded-[12px] w-full px-[15px] py-[5px] hover:bg-[#dce9ff] transition ease-in-out delay-50 ${isSelected === "/repos" ? "bg-[#dce9ff]" : "bg-white"}`}
         >
           <div className="flex justify-start items-center gap-2 ">
             <img className={`w-[27px]`} src={projects} />
@@ -32,7 +32,7 @@ export function Header() {
         <Link
           to="/dashboard"
           onClick={() => setSelect("/dashboard")}
-          className={`rounded-[12px] p-[5px] hover:bg-[#dce9ff] transition ease-in-out delay-50 ${isSelected === "/dashboard" ? "bg-[#dce9ff]" : "bg-white"}`}
+          className={`rounded-[12px]  px-[15px] hover:bg-[#dce9ff] transition ease-in-out delay-50 ${isSelected === "/dashboard" ? "bg-[#dce9ff]" : "bg-white"}`}
         >
           <div className="flex items-center gap-2 ">
             <img className={`w-[27px]`} src={dashboard} />
@@ -42,7 +42,7 @@ export function Header() {
         <Link
           to="/report"
           onClick={() => setSelect("/report")}
-          className={`rounded-[12px] p-[5px] hover:bg-[#dce9ff] transition ease-in-out delay-50 ${isSelected === "/report" ? "bg-[#dce9ff]" : "bg-white"}`}
+          className={`rounded-[12px]  px-[15px] hover:bg-[#dce9ff] transition ease-in-out delay-50 ${isSelected === "/report" ? "bg-[#dce9ff]" : "bg-white"}`}
         >
           <div className="flex items-center gap-2 ">
             <img className={`w-[27px]`} src={performance} />
@@ -52,7 +52,7 @@ export function Header() {
         <Link
           to="/guide"
           onClick={() => setSelect("/guide")}
-          className={`rounded-[12px] px-[5px] py-[5px] hover:bg-[#dce9ff] transition ease-in-out delay-50 ${isSelected === "/guide" ? "bg-[#dce9ff]" : "bg-white"}`}
+          className={`rounded-[12px]  px-[15px] py-[5px] hover:bg-[#dce9ff] transition ease-in-out delay-50 ${isSelected === "/guide" ? "bg-[#dce9ff]" : "bg-white"}`}
         >
           <div className="flex items-center gap-2 ">
             <img className={`w-[27px]`} src={flag} />
