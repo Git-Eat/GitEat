13c5dc46de3b182bf1e13af30ed5af4b0163fef5
이해루 <71335694+HAEROOL@users.noreply.github.com>
2025-02-19 13:33:31 +0900

    hotfix: 스크롤바 수정

--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -30,50 +30,57 @@ export function Login() {
       setUser(data);
     }
   }, [data, isLoading]);
+  useEffect(() => {
+    document.body.style.overflowY = "hidden";
+    return () => {
+      document.body.style.overflowY = "scroll";
+    };
+  }, []);
   if (isLoading) return <>loading</>;
-
   return (
-    <div className="snap-y snap-mandatory overflow-y-scroll h-screen">
-      <div className="w-[75%] mx-auto flex items-center h-screen justify-between snap-always snap-center scroll-smooth">
-        <section>
-          <div>
-            <h1 className="text-5xl font-bold mb-[25px] ">
-              GIT
-              <img className="inline w-[45px]" src={logo} alt="logo" />
-              EAT
-            </h1>
-            <article className="flex flex-col gap-[5px] text-[#5C5C5D] text-xl tracking-wide mb-[50px]">
-              <span>
-                <strong>Git-Eat</strong>은 코드리뷰 문화를 처음 접하는 주니어
-                개발자들에게
-              </span>
-              <span>
-                온보딩 플랫폼을 제공하는 서비스입니다.{" "}
-                <strong>AI 코드리뷰</strong>를 통해
-              </span>
-              <span>효율적인 코드리뷰를 진행해보세요 ! Let’s Git-Eat!</span>
-            </article>
-            <div className="w-[370px] flex flex-col gap-[18px]">
-              {/* <a
+    <div>
+      <div className="snap-y snap-mandatory overflow-y-scroll h-screen">
+        <div className="w-[75%] mx-auto flex items-center h-screen justify-between snap-always snap-center scroll-smooth">
+          <section>
+            <div>
+              <h1 className="text-5xl font-bold mb-[25px] ">
+                GIT
+                <img className="inline w-[45px]" src={logo} alt="logo" />
+                EAT
+              </h1>
+              <article className="flex flex-col gap-[5px] text-[#5C5C5D] text-xl tracking-wide mb-[50px]">
+                <span>
+                  <strong>Git-Eat</strong>은 코드리뷰 문화를 처음 접하는 주니어
+                  개발자들에게
+                </span>
+                <span>
+                  온보딩 플랫폼을 제공하는 서비스입니다.{" "}
+                  <strong>AI 코드리뷰</strong>를 통해
+                </span>
+                <span>효율적인 코드리뷰를 진행해보세요 ! Let’s Git-Eat!</span>
+              </article>
+              <div className="w-[370px] flex flex-col gap-[18px]">
+                {/* <a
               href="/loading"
               className="h-[71px] flex gap-[9px] bg-black text-white items-center justify-center font-[20px] font-semibold rounded-[30px]"
             >
               <img className="w-[55px]" src={githubLogo} alt="github_logo" />
               GitHub로 시작하기
             </a> */}
-              <button
-                onClick={gitLabLogin}
-                className="h-[71px] flex gap-[9px] bg-[#364CCA] text-white items-center justify-center font-[20px] font-semibold rounded-[30px]"
-              >
-                <img src={gitlabLogo} alt="gitlab_logo" />
-                SSAFY Git으로 시작하기
-              </button>
+                <button
+                  onClick={gitLabLogin}
+                  className="h-[71px] flex gap-[9px] bg-[#364CCA] text-white items-center justify-center font-[20px] font-semibold rounded-[30px]"
+                >
+                  <img src={gitlabLogo} alt="gitlab_logo" />
+                  SSAFY Git으로 시작하기
+                </button>
+              </div>
             </div>
-          </div>
-        </section>
-        <img src={side} alt="side" />
+          </section>
+          <img src={side} alt="side" />
+        </div>
+        <Landing />
       </div>
-      <Landing />
     </div>
   );
 }
